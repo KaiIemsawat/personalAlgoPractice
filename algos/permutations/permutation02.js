c6a3ce17b23cecab24ba3a0ff59dab8271f39682
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-06-11 21:43:13 -0400

    refresh permkutation

--- algos/permutations/permutation02.js
+++ algos/permutations/permutation02.js
@@ -1,22 +1,34 @@
 const permute = (numsArr, set = [], answer = []) => {
     if (!numsArr.length) {
         answer.push([...set]);
+        console.log("answer after push", answer);
     }
 
     for (let i = 0; i < numsArr.length; i++) {
-        const newNumsArr = numsArr.filter((_, index) => index !== i);
-        set.push(numsArr[i]);
-        // console.log("PUSH", set);
+        console.log("i : ", i);
+        // numsArr will always remain the same
 
+        const newNumsArr = numsArr.filter((_, index) => index !== i);
+        console.log("newNumsArr after .filter()", newNumsArr);
+
+        set.push(numsArr[i]);
+        console.log("Set after .push()", set);
+
+        console.log("calling permute");
         permute(newNumsArr, set, answer);
+        // When permute() is called with empty array. It also means that the argument array length is 0.
+        // Which means, 1. no for loop since i = argumentArray.length. 2. answer will push [...set]
+
+        // After finish the lowest layer of recursion, the progress will continue here
         set.pop();
-        // console.log("POP", set);
+        console.log("Set after .pop()", set);
+
+        console.log("============ for loop end ============");
     }
 
-    // console.log("ANSWER", answer);
     return answer;
 };
 
 let numArr = [1, 2, 3];
 
-console.log(permute(numArr));
+console.log("RESULT :: ", permute(numArr));
