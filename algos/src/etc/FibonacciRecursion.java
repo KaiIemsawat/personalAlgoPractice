b000b4a6a781183d60b707a9f4436f9cd40b55bb
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-06 21:02:44 -0400

    optimized Fibonacci using cache

--- algos/src/etc/FibonacciRecursion.java
+++ algos/src/etc/FibonacciRecursion.java
@@ -1,15 +1,30 @@
 package etc;
 
 public class FibonacciRecursion {
+
+    private static long[] fibonacciCache;
+
     public static void main(String[] args) {
-        int input = 9;
+        int input = 50;
+
+        fibonacciCache = new long[input + 1];
+
+        for (int i = 0; i<= input; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
         System.out.println(fibonacci(input));
     }
 
-    private static int fibonacci(int input) {
+    private static long fibonacci(int input) {
         if (input<=1) {
             return input;
         }
-        return (fibonacci(input-1) + fibonacci(input-2));
+
+        if (fibonacciCache[input] != 0) return fibonacciCache[input];
+
+        long nthFibonacciNumber = (fibonacci(input-1) + fibonacci(input-2));
+
+        fibonacciCache[input] = nthFibonacciNumber;
+        return nthFibonacciNumber;
     }
 }
