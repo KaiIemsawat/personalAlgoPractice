fc31309260e3348f7ed6fda9aa6f68dedfc7aeaa
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-13 19:28:14 -0400

    java linked list, singly, add, delete

---
+++ algos/dataStructure/javaCustomLinkedList/LL.java
@@ -0,0 +1,112 @@
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+    
+    public LL() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void insert(int val, int index) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        // Find node at 'index - 1' and assign it to 'temp'
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        // node will have the value of the input, its 'next' will be 'temp.next'
+        Node node = new Node(val, temp.next);
+        // then the next of 'temp' will be this 'node'
+        temp.next = node;
+
+        size++;
+    }
+
+    public Node getNode(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+
+        Node secondLast = getNode(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+
+        return val;
+    }
+
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ algos/dataStructure/javaCustomLinkedList/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.insertFirst(4);
+        list.insertFirst(3);
+        list.insertFirst(7);
+        list.insertFirst(21);
+        list.insertLast(99);
+        list.insert(234, 3);
+
+        System.out.println(list.deleteFirst());
+        System.out.println("--");
+        System.out.println(list.deleteLast());
+        System.out.println("--");
+
+        list.display();
+    }
+}
