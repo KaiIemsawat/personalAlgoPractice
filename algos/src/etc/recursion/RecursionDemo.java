62d3aa2da6de9170b07ec09c9e162f168794a7eb
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-12 21:38:27 -0400

    refresh recursion

---
+++ algos/src/etc/recursion/RecursionDemo.java
@@ -0,0 +1,19 @@
+package etc.recursion;
+
+public class RecursionDemo {
+
+    public static void main(String[] args) {
+        sayHi(3);
+    }
+
+
+    private static void sayHi(int count) {
+
+        System.out.println("Hi");
+
+        if (count <= 1) {
+            return;
+        }
+        sayHi(count - 1);
+    }
+}
