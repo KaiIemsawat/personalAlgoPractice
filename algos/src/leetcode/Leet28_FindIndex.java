e1a25f016b3bde362d3dd49807cfa77dfd91941a
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-19 20:04:35 -0400

    more leetcode problems

---
+++ algos/src/leetcode/Leet28_FindIndex.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class Leet28_FindIndex {
+    public static void main(String[] args) {
+        System.out.println(strStr("sadlllsad", "sadd"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
---
+++ algos/src/leetcode/Leet35_SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Leet35_SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+
+        System.out.println(searchInsert(nums, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+            else if (nums[i] > target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
