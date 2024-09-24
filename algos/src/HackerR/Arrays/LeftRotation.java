78744973d76a3418b9a278903d952cbd4ce88db0
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-24 11:26:00 -0400

    hackerrank arrays/left rotation - fixed

--- algos/src/HackerR/Arrays/LeftRotation.java
+++ algos/src/HackerR/Arrays/LeftRotation.java
@@ -13,22 +13,21 @@ public class LeftRotation {
         arr.add(4);
         arr.add(5);
 
-        System.out.println(rotateLeft(4, arr));
+        System.out.println(rotateLeft(3, arr));
     }
 
     public static List<Integer> rotateLeft(int d, List<Integer> arr) {
-        while (d > 0) {
-            int temp = arr.get(0);
-            for (int i = 0; i< arr.size()-1; i++) {
-                arr.set(i, arr.get(i+1));
-                if (i == arr.size()-2) {
-                    arr.set(arr.size()-1, temp);
-                }
-            }
-            d--;
-        }
-        return arr;
 
+        List<Integer> ret = new ArrayList<>();
+
+        for (int i = d; i < arr.size(); i++) {
+            ret.add(arr.get(i));
+        }
+
+        for (int i = 0; i < d; i++) {
+            ret.add(arr.get(i));
+        }
+        return ret;
 
     }
 }
