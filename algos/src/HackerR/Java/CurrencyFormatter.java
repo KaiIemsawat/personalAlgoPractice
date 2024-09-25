34456248d1fddbc33cfcca1553fbb1832c48b259
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-24 20:18:59 -0400

    hackerrank java/CurrencyConverter

---
+++ algos/src/HackerR/Java/CurrencyFormatter.java
@@ -0,0 +1,29 @@
+package HackerR.Java;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat n1 = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = n1.format(payment);
+
+        NumberFormat n2 = NumberFormat.getCurrencyInstance(new Locale("en","IN"));
+        String india = n2.format(payment);
+
+        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = n3.format(payment);
+
+        NumberFormat n4 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = n4.format(payment);
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
